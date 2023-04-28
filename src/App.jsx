fe75c459264a234b8799082fb70cb95d19770eb9
Kisha Aggarwal <kishaaggarwal195@gmail.com>
2023-04-28 13:04:46 +0000

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kisha Aggarwal",
+  title: "Frontend Developer & Solution-Oriented Problem Solver",
+  email: "kishaaggarwal195@gmail.com",
+  gitHub: "kishaaggarwal32",
+  instagram: "kishaaggarwal195",
+  linkedIn: "kishaaggarwal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "kishaaggarwal",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
